6a71ee2464068638780e39a3ba240a2081842088
juliachen319 <jchen@team4099.com>
2022-10-15 22:45:07 -0400

    add multiprocessing and render template

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import os
+from multiprocessing import Process, Value
+import time
 from twilio.rest import Client
 from geopy.geocoders import Nominatim
 import geocoder
@@ -6,7 +8,7 @@ import geocoder
 from flask import Flask, render_template, request, session, redirect, url_for
 app = Flask(__name__)
 
-users = {"+12407510959": (38.9658, -77.068)}
+users = {"+12407510959": [(38.9658, -77.068), "bus"]}
 
 #set up twilio client
 account_sid = os.environ['TWILIO_ACCOUNT_SID']
@@ -25,7 +27,7 @@ def form():
         dest_loc = loc.geocode(dest)
         coords = (dest_loc.latitude, dest_loc.longitude)
         users = {phone: coords}
-    
+    return render_template('home.html')
 
 @app.route('/call', methods=['GET', 'POST'])
 def call():
@@ -35,6 +37,17 @@ def call():
                         from_='+18507883830'
                     )
 
+def big_loop():
+    i = 0
+    while(True):
+        print(i)
+        i += 1
+        time.sleep(20)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    print("start")
+    recording_on = Value('b', True)
+    p = Process(target=big_loop)
+    p.start()  
+    app.run(debug=True, use_reloader=False)
+    p.join()
